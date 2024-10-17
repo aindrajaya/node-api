03246b61bef1b8910f9844a5e11eb9dc40fe701d
aindrajaya <arista.indrajaya@gmail.com>
2024-10-17 13:14:59 +0700

    change the file folder to become more modular

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-// This API will use Express framework
+// Import the service from the server file
+const app = require('./server');
+// const serverVanilla = require('./vanilla/index.js');
+
+/**
+ * Notes:
+ * 1. The app.listen() method is used to start the server listening for incoming requests.
+ * 2. The method takes two arguments: the port number and a callback function.
+ * 3. The port number is the port on which the server will listen for incoming requests.
+ * 4. The callback function is executed when the server starts listening for incoming requests.
+ * 5. The callback function can be used to perform any necessary setup or logging.
+ * 6. The server will continue running until it is stopped or an error occurs.
+ */
+
+// Start the server
+app.listen(3000, () => {
+    // Log a message to the console
+    console.log('Server is running on http://localhost:3000');
+})
---
+++ src/server.js
@@ -0,0 +1,47 @@
+// This API will use Express framework
+const express = require('express');
+
+// Import the path module
+const path = require('path');
+
+// Create an Express application
+const app = express();
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+/**
+ * 1. The app.get() method is used to define a route handler for GET requests.
+ * 2. The method takes two arguments: the route path and a callback function.
+ * 3. The route path is a string that defines the URL path for the route handler.
+ * 4. The callback function takes two arguments: req (request) and res (response).
+ * 5. The req object contains information about the request, such as the URL, headers, and body.
+ * 6. The res object is used to send a response back to the client.
+ * 7. The res.status() method is used to set the HTTP status code for the response.
+ * 8. The res.json() method is used to send a JSON response back to the client.
+ * 
+ */
+app.get('/', (req, res) => {
+    // Log a message to the console
+    console.log('GET request received use Express framework');
+
+    // Set the HTTP status code for the response
+    res.status(200);
+
+    // Send a JSON response back to the client
+    res.json({message: 'Hello World use Express framework'});
+})
+
+app.get('/about', (req, res) => {
+    // Log a message to the console
+    console.log('GET request received to access About page use Express framework');
+
+    // Set the HTTP status code for the response
+    res.status(200);
+
+    // Send a JSON response back to the client
+    res.sendFile(path.resolve('./src/public/about.html'));
+})
+
+// Export the Express application
+module.exports = app;
